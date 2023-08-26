c91774f10e71a9f07ec6641e9ae5752732cde5ab
morphage32 <mwigand32@yahoo.com>
2023-08-25 18:57:52 -0700

    Spec files added with tests for the major core functions within the Connect Four game.

--- spec/spec_player.rb
+++ spec/spec_player.rb
@@ -1,13 +1,22 @@
 require_relative '../lib/player.rb'
 
 describe Player do
-    describe "#initialize" do
-        it "can create a new player with a name and symbol" do
-            name = "Tester"
-            symbol = "$"
-            new_player = described_class.new(name, symbol)
-            expect(new_player.name).to eq("Tester")
-            expect(new_player.symbol) to eq("$")
-        end
+  describe "#initialize" do
+    it "can create a new player with a custom name" do
+      name = "Tester"
+      new_player = described_class.new(name)
+      expect(new_player.name).to eq("Tester")
     end
+  end
+
+  describe "#set_symbol" do
+    it "assigns a symbol when only one character is input" do
+      name = "Tester2"
+      new_player2 = described_class.new(name)
+      allow(new_player2).to receive(:gets).and_return("$")
+      allow(new_player2).to receive(:puts).and_return("")
+      new_player2.set_symbol
+      expect(new_player2.symbol).to eq("$")
+    end
+  end
 end
