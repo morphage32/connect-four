3f191f92f92dfc23718e52ba73c350187cdc0f64
morphage32 <mwigand32@yahoo.com>
2023-08-25 18:59:07 -0700

    Function created to ensure the user assigns a single character as the symbol they can place on the game board.

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,14 @@
 class Player
-    attr_reader :name, :symbol
-    def initialize(name, symbol)
-        @name = name
-        @symbol = symbol
+  attr_reader :name, :symbol
+  def initialize(name)
+    @name = name
+    @symbol = ""
+  end
+
+  def set_symbol()
+    until @symbol.length == 1 do
+      puts "Please enter a single character for #{name}:"
+      @symbol = gets.chomp
     end
+  end
 end
